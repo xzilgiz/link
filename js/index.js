5fa40e5e635523a20584fb920d87339de2779dfd
ilgiz <xzilgiz@mail.ru>
2013-03-27 00:01:29 +0400

    выплывающее меню

--- js/index.js
+++ js/index.js
@@ -20,14 +20,27 @@ $(document).ready(function () {
   //    $('#loader').removeClass('visibleon').addClass('visibleoff');      
   //  });
 
-});
-
-$(window).resize(function() {
-  var h = Math.max($(window).height(), $(document).height()) - ($('#header').height() + $('#footer').height());
-      
+  $(window).resize(function() {
+    var h = Math.max($(window).height(), $(document).height()) - ($('#header').height() + $('#footer').height());      
       $('#content').height(h);
       $('#left').height(h);
       $('#right').height(h);
-      $('#center').height(h);
-      
+      $('#center').height(h);      
+  });
+
+  $("#left").hoverIntent(
+    function () {
+      $(this).animate({width: "250px",opacity: 0.8}, 500);      
+      $("#center").animate({marginLeft: "250px",marginRight: "0px"}, 500);
+      $("#right").animate({width: "0px",opacity: 0.0}, 500);      
+    },
+    function () {
+      $(this).animate({width: "50px",opacity: 0.2}, 200 );      
+      $("#center").animate({marginLeft: "50px",marginRight: "200px"}, 200 );      
+      $("#right").animate({width: "200px",opacity: 0.8}, 200 );
+    }
+  );
+
 });
+
+
